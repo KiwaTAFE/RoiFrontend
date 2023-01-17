60d19856435e9b47703165a66c1e1d4f9c1212b1
Murrumba O'New-Dawn <murrumba.onewdawn@studytafensw.edu.au>
2023-01-17 18:03:09 +1100

    set roi color constants

--- constants/Colours.js
+++ constants/Colours.js
@@ -1,10 +1,15 @@
 
 // Set up some colours to use in this file (consult style guide)
 const myColours = {
-  colour1:       '#008B8B',
-  colour2:       '#20B2AA',
-  colour3:       '#1E90FF',
-}
+  roiRed:         '#941a1d',
+  roiCharcoal:    '#262626',
+  roiGrey:        '#595959',
+  roiWhite:       '#ffffff',
+  roiBurntOrange: '#c64c38',
+  roiLightOrange: '#cb6d4f',
+  roiMidGrey:     '#3b3b3b',
+  roiLightGrey:   '#D9D9D9',
+};
 
 // Export all the colours used by the app
 export default {
